a14c57cbc0eb71fd9f780526efdcdbd22519b5f7
miko1034 <84585119+miko1034@users.noreply.github.com>
2024-04-12 19:07:49 +0100

    completed code

---
+++ scan.py
@@ -0,0 +1,50 @@
+import requests
+
+# add the url you want to scan below
+# no slash at the end, and make sure to add http:// or https://
+# e.g: https://example.com
+
+BASE_URL = "https://harlington.org" 
+
+#functions
+def find_between( s, first, last ):
+    try:
+        start = s.index(first) + len(first)
+        end = s.index(last, start)
+        return s[start:end]
+    except ValueError:
+        return ""
+
+def checkResponses(urls):
+    response_200 = []
+    for i in range(len(urls)):
+        r = requests.get(url=urls[i])
+        if r.status_code == 200:
+            response_200.append(urls[i])
+            print(f"url {i} found to be valid")
+    return response_200
+
+#sets some needed variables
+endpoint = f"{BASE_URL}/wp-json/"
+scraped_urls = []
+
+#response from website gotten
+r = requests.get(url=endpoint)
+response = r.text.split(",")
+for i in range(len(response)): # finds all urls in wp-json
+    current_url = find_between(response[i], '"href":"', '"')
+    current_url = current_url.replace('\\', '')
+    if current_url == '':
+        continue
+    scraped_urls.append(current_url)
+
+#print(scraped_urls)
+
+#creates a list of valid urls
+r200 = checkResponses(scraped_urls)
+print(r200)
+
+#writes them to file
+with open("urls.txt", "+a") as f:
+    for i in range(len(r200)):
+        f.write(f"{r200[i]}\n")
