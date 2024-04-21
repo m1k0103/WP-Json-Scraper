29bb8da80e3bcf2a2555f3e319b9bd1810add545
miko1034 <84585119+miko1034@users.noreply.github.com>
2024-04-21 21:06:04 +0100

    improved code

--- scan.py
+++ scan.py
@@ -4,7 +4,7 @@ import requests
 # no slash at the end, and make sure to add http:// or https://
 # e.g: https://example.com
 
-BASE_URL = "" # ********** ADD BASE URL HERE **********
+BASE_URL = "https://harlington.org" # ********** ADD BASE URL HERE **********
 
 #functions
 def find_between( s, first, last ):
@@ -21,30 +21,33 @@ def checkResponses(urls):
         r = requests.get(url=urls[i])
         if r.status_code == 200:
             response_200.append(urls[i])
-            print(f"url {i} found to be valid")
     return response_200
+    
+def scrape(base_url):
+    #sets some needed variables
+    endpoint = f"{BASE_URL}/wp-json/"
+    scraped_urls = []
+    
+    #response from website gotten
+    r = requests.get(url=endpoint)
+    response = r.text.split(",")
+    for i in range(len(response)): # finds all urls in wp-json
+        current_url = find_between(response[i], '"href":"', '"')
+        current_url = current_url.replace('\\', '')
+        if current_url == '':
+            continue
+        scraped_urls.append(current_url)
+    
+    #print(scraped_urls)
+    
+    #creates a list of valid urls
+    r200 = checkResponses(scraped_urls)
+    print(r200)
+    
+    #writes them to file
+    with open(f"{BASE_URL.strip("http://").strip("https://")}.txt", "+a") as f:
+        for i in range(len(r200)):
+            f.write(f"{r200[i]}\n")
 
-#sets some needed variables
-endpoint = f"{BASE_URL}/wp-json/"
-scraped_urls = []
 
-#response from website gotten
-r = requests.get(url=endpoint)
-response = r.text.split(",")
-for i in range(len(response)): # finds all urls in wp-json
-    current_url = find_between(response[i], '"href":"', '"')
-    current_url = current_url.replace('\\', '')
-    if current_url == '':
-        continue
-    scraped_urls.append(current_url)
-
-#print(scraped_urls)
-
-#creates a list of valid urls
-r200 = checkResponses(scraped_urls)
-print(r200)
-
-#writes them to file
-with open("urls.txt", "+a") as f:
-    for i in range(len(r200)):
-        f.write(f"{r200[i]}\n")
+scrape(BASE_URL)
